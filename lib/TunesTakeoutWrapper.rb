6e1691c75398503c4bf853c0be199d3086cdfd25
Anna Wilson <annie.marie.mason@gmail.com>
2016-05-17 11:31:01 -0700

    TunesTakeoutWrapper returns instance of suggestion with readable attributes

--- lib/TunesTakeoutWrapper.rb
+++ lib/TunesTakeoutWrapper.rb
@@ -1,20 +1,18 @@
 require 'httparty'
 
-# TunesTakeoutWrapper:
-# Wraps interactions with the Tunes & Takeout API
-# by leveraging the HTTParty gem.
-
 class TunesTakeoutWrapper
   BASE_URL = "https://tunes-takeout-api.herokuapp.com/"
-  attr_reader :suggestion
+  attr_reader :id, :food_id, :music_id, :music_type
 
   def initialize(data)
-    all_suggestions = data["suggestions"]
-    @suggestion = all_suggestions[rand(0..all_suggestions.length)]
+    @id = data["suggestions"][0]["id"]
+    @food_id = data["suggestions"][0]["food_id"]
+    @music_id = data["suggestions"][0]["music_id"]
+    @music_type = data["suggestions"][0]["music_type"]
   end
 
   def self.find(item)
-    data = HTTParty.get(BASE_URL + "/v1/suggestions/search?query=#{item}/").parsed_response
+    data = HTTParty.get(BASE_URL + "/v1/suggestions/search?query=#{item}&limit=1/").parsed_response
     self.new(data)
   end
 
