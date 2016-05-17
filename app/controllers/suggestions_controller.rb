9019e345c713f20245b66f02a364a874ecbd61e8
Anna Wilson <annie.marie.mason@gmail.com>
2016-05-17 16:50:40 -0700

    trying to get the tunestakeout wrapper to display on suggestions#index. in an error state at the moment

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -3,6 +3,10 @@ require 'TunesTakeoutWrapper'
 class SuggestionsController < ActionController::Base
 
   def index
+    if params["search_term"].present?
+      search_term = params["search_term"]
+      @suggestion = TunesTakeoutWrapper.find(search_term)
+    end
   end
 
   def favorites
