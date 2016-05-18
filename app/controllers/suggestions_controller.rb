41a9dbf40fb3f2c4166ebed7eb0f559f6e1c202f
Anna Wilson <annie.marie.mason@gmail.com>
2016-05-18 09:40:26 -0700

    search term will produce music item on index yaaaay

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,11 +1,13 @@
 require 'TunesTakeoutWrapper'
+require 'spotifyitem.rb'
 
 class SuggestionsController < ActionController::Base
 
   def index
     if params["search_term"].present?
       search_term = params["search_term"]
-      @suggestion = TunesTakeoutWrapper.find(search_term)
+      @pairing = TunesTakeoutWrapper.find(search_term)
+      @music_suggestion = SpotifyItem.find(@pairing.music_type,@pairing.music_id)
     end
   end
 
--- app/models/spotifyitem.rb
+++ app/models/spotifyitem.rb
@@ -8,12 +8,13 @@ class SpotifyItem
     @type = data.type
     @name = data.name
     @url = data.external_urls.values[0] #extracts string of url
-    @image_url = data.images[0]["url"] #extracts string of img that is 640 hgt
+    # @image_url = data.images[0]["url"] ||= nil #extracts string of img that is 640 hgt
+    #image needs reworking, doesnt work for tracks. has different pathing.
   end
 
-  def self.find(id)
+  def self.find(music_type,music_id)
     if music_type == "track"
-      data = RSpotify::Track.find('music_id')
+      data = RSpotify::Track.find(music_id)
     elsif music_type == "album"
       data = RSpotify::Album.find('music_id')
     elsif music_type == "artist"
@@ -24,5 +25,9 @@ class SpotifyItem
 
     self.new(data)
   end
+
+
+#  => "track"
+#  => "0BjkSCLEHlcsogSeDim01W"
 
 end
