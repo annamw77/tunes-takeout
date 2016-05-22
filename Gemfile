2c39e95a9937ba44329087834c6d56b894cef799
Anna Wilson <annie.marie.mason@gmail.com>
2016-05-21 19:45:37 -0700

    moved sessions[:favorites] declaration from session initlizer to application controller.... much struggle was had. favorites and unfavorites bug is now fixed, it was calling the id instead of the username.

--- Gemfile
+++ Gemfile
@@ -38,7 +38,6 @@ gem 'omniauth'
 gem 'omniauth-oauth2', '~> 1.3.1'
 
 
-
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
+require 'TunesTakeoutWrapper'
+
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+   before_filter :find_favorites
+   before_action :find_favorites
+
+  def find_favorites
+    if session[:user_id].present?
+      @user = User.find(session[:user_id])
+      session[:favorites] = TunesTakeoutWrapper.check_for_favorites(@user.uid)
+    end
+  end
+
 end
